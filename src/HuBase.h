2949372d4016d7cb4e20bd2cd51c7d7a4f5d23fa
lwz <wenzhe_l@163.com>
2017-02-07 16:32:22 +0800

    2017/02/07

--- src/HuBase.h
+++ src/HuBase.h
@@ -6,8 +6,62 @@
  *
  * -------------------------------
  *
- * This file contains apis of HuBase.
+ * Apis of HuBase.
  *
  **/
+
+
+#ifndef _HUBASE_H_
+#define _HUBASE_H_
+
+#include <sys/type.h>
+/**
+ *
+ * Naming: usually start with "huBase_"
+ *
+ **/
+
+/**
+ *
+ * define own standard types.
+ *
+ **/
+/* unsigned */
+typedef __uint8_t h_u8;
+typedef __uint16_t h_u16;
+typedef __uint32_t h_u32;
+typedef __uint64_t h_u64;
+
+/* signed */
+typedef __int8_t h_i8;
+typedef __int16_t h_i16;
+typedef __int32_t h_i32;
+typedef __int64_t h_i64;
+
+/**
+ *
+ * IO_method "sets".
+ * The abstraction of io methods.
+ *
+ * So that different files can use different method "strategies".
+ *
+ **/
+typedef struct huBase_io_methods {
+    int (*xClose)( huBase_file* file );                                             /* Close the file */
+    int (*xRead)( huBase_file* file, void* buff, int amount, h_i64 offset );        /* Read the file */
+    int (*xWrite)( huBase_file* file, const void* buff, int amount, h_i64 offset ); /* Write buff */
+    int (*xTrunc)( huBase_file* file, h_i64 size );                                 /* Truncate file */
+    int (*xSync)( huBase_file* , int flag );                                        /* Sync file */
+    int (*xLock)( huBase_file* file, int flag );
+    int (*xUnlock)( huBase_file* file, int flag );
+    int (*xFileSize)( huBase_file* file, h_i64* size );                             /* get the file size */
+    int (*xFileControl)( huBase_file* file, int opcode, void* pArg );               /* file control */
+} huBase_io_methods;
+
+typedef struct huBase_file {
+    const huBase_io_methods* pMethods;   //The detailed implementation leaves to subclasses.
+} huBase_file;
+
+#endif /* _HUBASE_H_*/
 
 
--- src/os.h
+++ src/os.h
@@ -1,4 +1,5 @@
 /**
+ * 
  * os.h
  *
  * Since: 2017-02-06
@@ -9,5 +10,45 @@
  * The interfaces of OS layer.
  *
  **/
+
+#ifndef _HUBASE_OS_H_
+#define _HUBASE_OS_H_
+
+#include "HuBase.h"
+
+/**
+ * This database cannot run on Windows.
+ **/
+#if defined(_WIN32) || defined(_WIN64)
+ #error "HuBase cannot run on windows"
+#endif 
+
+/**
+ *
+ * Unlike sqlite3, I am not intended to use a 
+ * virtual file system to represent different lock 
+ * strategies.
+ *
+ * So, the upper layer will manipulate files using huBase_file.
+ *
+ **/
+
+void huBaseClose( huBase_file* );                         /* Close the file */
+int huBaseRead( huBase_file*, void* buff, int, h_i64 );   /* Read the file */
+int huBaseWrite( huBase_file*, const void*, int, h_i64 ); /* Write buff */
+int huBaseTruncate( huBase_file*, h_i64 );                /* Truncate file */
+int huBaseSync( huBase_file*, int );                      /* Sync file */
+int huBaseFileSize( huBase_file*, h_i64* );
+int huBaseLock( huBase_file*, int );
+int huBaseUnlock( huBase_file*, int );
+int huBaseFileControl( huBase_file*, int, void* );        /* file control */
+
+/**
+ * Open the file on the path and return the huBase_file object. 
+ * use the flag to choose different io_methods strategies for the file.
+ **/
+int huBaseOpen( const char* path, huBase_file* fill, int openFlag );
+
+#endif /* _HUBASE_OS_H_*/
 
 
